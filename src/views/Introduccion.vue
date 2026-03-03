40de4ca86a291b0625ddf49dba72104859176fce
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2026-03-03 12:26:44 -0500

    avance

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -23,8 +23,8 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/wCoeunseiQI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video. Propiedades y aplicaciones de los aceites esenciales de plantas aromáticas
 
 </template>
 
